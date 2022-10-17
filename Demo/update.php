0b002b45714448319faf5f0724b97300b62c6e21
Kris-fynn <67267786+Kris-fynn@users.noreply.github.com>
2022-10-17 10:27:44 +0200

    CRUD Added by Kris

--- Demo/update.php
+++ Demo/update.php
@@ -9,6 +9,79 @@
         $gender = $_POST['gender'];
         $password =$_POST['password'];
      
-        $sql = "UPDATE"
-    }
-?>
+        $sql = "UPDATE 'users' SET 'firstname' = '$firstname', 'lastname' = '$lastname', 'email' = '$email', 'password' = '$password', 'gender' = '$gender' WHERE 'id' = 'user_id'";
+
+        $result = $conn->query($sql);
+
+        if($result == TRUE){
+            echo "Record Update Successfully";
+        }
+        else{
+            echo "Error:" .$sql. "<br" . $conn->error;
+        }
+
+    }  
+
+    if (isset($_GET['id'])) {
+        $user_id = $_GET['id'];
+
+        $sql = "SELECT *FROM 'user' WHERE 'id' = '$user_id'";
+        
+        $result = $conn->query($sql);
+
+        if($result->num_rows > 0){
+            while($row = $result->fetch_assoc()){
+
+                $firstname = $row['firstname'];
+                $lastname = $row['lastname'];
+                $email = $row['email'];
+                $password = $row['password'];
+                $gender = $row['gender'];
+                $id = $row['id'];
+
+            }
+         <!DOCTYPE html>
+         <html>
+         <body>
+           <h2>User Update Form</h2>
+
+           <from action="" method="post">
+               <fieldset>
+                <legend>Personal information:</legend>
+                First name:<br>
+                <input type="text" name="firstname" value="<?php echo $first_name; ?>" >
+                <input type="hidden" name = "user_id" value= "<?php echo $id; ?>">
+                <br>
+
+                Last name:<br>
+                <input type = "text" name = "lastname" value = "<?php echo $last_name; ?>">
+                <br>
+
+                Email:<br> 
+                <input type = 'email' name = "email" value = "<? php echo $email; ?>">
+                <br>
+
+                Password:<br>
+                <input type = "password" name = "password" value = "<?php echo $password; ?>">
+                <br>
+
+                Gender:<br>
+                <input type = "radio" name = "gender" value = "male" <?php if ($gender == 'Male'){ echo "checked;"} ?>>Male
+                <input type = "radio" name = "gender" value = "Female" <?php if ($gender == 'Female'){ echo "chacked;"} ?>>Female
+                <br><br>
+
+                <input type = "submit" value = "update" name = "update">
+                
+            
+               </fieldset>
+           </from>
+          </body> 
+          </html>
+          <?php
+          
+          ?>
+        
+        }
+
+    } 
+?>    
